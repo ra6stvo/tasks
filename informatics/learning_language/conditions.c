17c00ecea4ecd0158ca3458c34e9c40e96ecb3cc
ra6stvo <ra6stvo@gmail.com>
2024-02-27 16:15:57 +0000

    add is_triangle_exists task

--- informatics/learning_language/conditions.c
+++ informatics/learning_language/conditions.c
@@ -15,3 +15,22 @@ void leap_year()
 		printf("NO\n");
 	}
 }
+
+// https://informatics.msk.ru/mod/statements/view.php?id=276&chapterid=295#1
+void is_triangle_exists()
+{
+	int a;
+	int b;
+	int c;
+
+	scanf("%d%d%d", &a, &b, &c);
+
+	if ((a + b > c) && (a + c > b) && (b + c > a))
+	{
+		printf("YES\n");
+	}
+	else
+	{
+		printf("NO\n");
+	}
+}
