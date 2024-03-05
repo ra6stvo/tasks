b3dc8f8c84307539c8e7b9e170868c37370d1496
ra6stvo <ra6stvo@gmail.com>
2024-03-05 14:11:28 +0000

    add testing system

--- informatics/learning_language/conditions.c
+++ informatics/learning_language/conditions.c
@@ -34,3 +34,107 @@ void is_triangle_exists()
 		printf("NO\n");
 	}
 }
+
+// https://informatics.msk.ru/mod/statements/view.php?id=276&chapterid=2959#1
+void the_sign_of_the_number()
+{
+	int x;
+
+	scanf("%d", &x);
+
+	if (x > 0) {
+		printf("1\n");
+	} 
+	else if (x < 0) {
+		printf("-1\n");
+	}
+	else {
+		printf("0\n")
+	}
+}
+
+// https://informatics.msk.ru/mod/statements/view.php?id=276&chapterid=293#1
+void Which_of_the_numbers_is_bigger()
+{
+	int a;
+	int b;
+
+	scanf("%d%d", &a, &b);
+
+	if (a > b) {
+		printf("1\n"); 
+	}
+	else if (b > a) {
+		printf("2\n");
+	}
+	else {
+		printf("0\n")
+	}
+}
+
+// https://informatics.msk.ru/mod/statements/view.php?id=276&chapterid=294#1
+void maximum_of_three()
+{
+	int a;
+	int b;
+	int c;
+	int max = a;
+
+	scanf("%d%d%d", &a, &b, &c);
+
+	if (c > max) {
+		max = c;
+	}
+	else if (b > max) {
+		max = b
+	}
+
+	printf("%d\n", max);
+}
+
+// https://informatics.msk.ru/mod/statements/view.php?id=276&chapterid=260#1
+void the_equation()
+{
+	int a;
+	int b;
+// ax + b = 0
+	scanf("%d%d", &a, &b);
+
+	if (a == 0) and (b == 0) {
+		printf("INF\n");
+	}
+	else if (a == 0) and (b!=0) {
+		printf("NO\n");
+	}
+	else {
+		printf("%d\n", -b/n);
+	}
+}
+
+// https://informatics.msk.ru/mod/statements/view.php?id=276&chapterid=292#1
+void maximum_of_two_numbers()
+{
+	int a;
+	int b;
+
+	scanf("%d%d", &a, &b);
+
+	if (a > b) {
+		printf("%d\n", a);
+	}
+	else {
+		printf("%d\n", b);
+	}
+}
+
+// https://informatics.msk.ru/mod/statements/view.php?id=276&chapterid=2960#1
+void testing_system()
+{
+
+	int student;
+	int system;
+
+	scanf("%d%d", &student, &system);
+
+	// не понял, как делать
+}
