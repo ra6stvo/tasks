1b0e021a1749d342e0f970b468f9972ea345b13d
ra6stvo <156512948+ra6stvo@users.noreply.github.com>
2024-08-01 11:27:27 +0300

    add even_num_1st task

---
+++ informatics/learning_language/linear_data_structure/even_num_1st.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int n;
+    
+    scanf("%d", &n);
+    
+    int *arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) 
+    {
+        if (arr[i] % 2 == 0) 
+        {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) 
+    {
+        if (arr[i] % 2 != 0) 
+        {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    printf("\n");
+    
+    free(arr);
+    
+    return 0;
+}
